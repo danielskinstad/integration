d75f00b34702a795292ff1f1c976d6f50bc9caf9
Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
2025-10-14 13:00:24 +0200

    test: debug
    Revert "fix: fine-tune docker images priorities to fix failing test:integration:1 pipeline"
    
    This reverts commit 856982e0c331d26f12a8f6c289bf867e3ddd12bb.

--- testutils/infra/container_manager/docker_manager.py
+++ testutils/infra/container_manager/docker_manager.py
@@ -12,11 +12,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 import subprocess
-import logging
 
 from .base import BaseContainerManagerNamespace
-
-logger = logging.getLogger()
 
 
 class DockerNamespace(BaseContainerManagerNamespace):
@@ -31,18 +28,7 @@ class DockerNamespace(BaseContainerManagerNamespace):
 
     def execute(self, container_id, cmd):
         cmd = ["docker", "exec", "{}".format(container_id)] + cmd
-        try:
-            result = subprocess.run(cmd, check=True, capture_output=True, text=True)
-            ret = result.stdout.strip()
-        except subprocess.CalledProcessError as e:
-            logger.error(
-                f"Command failed with exit code {e.returncode}. "
-                f"Command attempted: {e.cmd}\n"
-                f"Captured STDOUT:\n{e.stdout}\n"
-                f"Captured STDERR:\n{e.stderr}\n"
-                f"-------------------------"
-            )
-            raise  # re-raise as we want the test to end with an error
+        ret = subprocess.check_output(cmd).decode("utf-8").strip()
         return ret
 
     def cmd(self, container_id, docker_cmd, cmd=[]):
